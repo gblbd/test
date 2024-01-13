ee6ab326f425338ac337adaccdf0b2bac2afba1f
Abdulla Al Mued <abdullaalmuid101@gmail.com>
2024-01-13 11:20:00 +0600

    first commit

--- lib/Api/Constants.dart
+++ lib/Api/Constants.dart
@@ -5,4 +5,5 @@ class Constants{
   static String SUB_CATEGORY_URL = "http://52.77.83.48:8083/api/v/1.0.0/sofol/public/advertisement/niche-category/get-all/655490b838619a1a2cedc53f";
   static String BASE_URL = "http://52.77.83.48:8083";
 
+
 }
